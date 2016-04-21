46cae96a1c46ad69a5fddb9ed4afde34117803fb
unixfox <mimidevos2@gmail.com>
2016-04-21 21:12:30 +0200

    Maybe final of rework

--- apply.bash
+++ apply.bash
@@ -1,35 +1,53 @@
 #!/bin/bash
 
-$remoteurlgit="git config --get remote.origin.url"
+. /lib/lsb/init-functions
+
+remoteurlgit="$(git config --get remote.origin.url)"
 
 log_daemon_msg "Setting up basic sources and preferences..."
 
-if [ "$remoteurlgit" == "https://github.com/unixfox/APT-preferences-for-Debian.git" ] || [ "$remoteurlgit" == "git@github.com:unixfox/APT-preferences-for-Debian-8.git" ]
+if ! type "sudo" > /dev/null; then
+  if [[ $EUID -ne 0 ]]; then
+   log_failure_msg "You have to run this script as root for installing necessary depencies!"
+   exit 1
+  fi
+  apt-get install -qq -y sudo
+fi
+
+if [ "$remoteurlgit" != "https://github.com/unixfox/APT-preferences-for-Debian.git" ] || [ "$remoteurlgit" != "git@github.com:unixfox/APT-preferences-for-Debian-8.git" ]
 then
 
 if ! type "git" > /dev/null; then
-  apt-get install -y git > /dev/null
+  sudo apt-get install -qq -y git
 fi
 
-log_progress_msg "Cloning the repo"
+log_progress_msg "Cloning the repo... "
 
-git clone https://github.com/unixfox/APT-preferences-for-Debian.git /etc/apt/apt-preferences-git > /dev/null
+git clone https://github.com/unixfox/APT-preferences-for-Debian.git /etc/apt/apt-preferences-git >/dev/null 2>&1
 cd /etc/apt/apt-preferences-git
 fi
 
-log_progress_msg "Coping sources and preferences"
+log_progress_msg "Coping sources and preferences..."
 
 sudo cp -R sources.list.d/* /etc/apt/sources.list.d/
 sudo cp -R preferences.d/* /etc/apt/preferences.d/
 
 log_end_msg 0 || true
 
-read -p $"\e[93m[Warning] \e[39mDo you want to edit 'sources.list' to remove unnecessary repositories (y/n)?" -n 1 -r
+read -p $"Do you want to edit 'sources.list' to remove unnecessary repositories (y/n)?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    nano /etc/apt/sources.list
+    sudo nano /etc/apt/sources.list
 fi
 
-echo "Updating repositories..."
-apt-get update
+log_daemon_msg "Updating repositories..."
+
+sudo apt-get -qq update
+if [[ $? > 0 ]]
+then
+    log_failure_msg "Error on refreshing repositories, check error by executing 'apt-get update'!"
+    exit
+else
+    log_end_msg 0 || true
+fi
